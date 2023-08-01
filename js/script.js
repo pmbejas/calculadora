54920568b224795e61f8d68ebfaa2b51c3a709cf
pmbejas <pmbejas@gmail.com>
2023-07-31 23:10:49 -0300

    Se optimizo el código, se agrego las referencias de las operaciones, se reorganizaron los botones, se puso pie de pagina

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ const handleKeyPress = (event) => {
     }
 }
 
-const handleBackspace =(event) => {
+const handleKeyDown = (event) => {
     if (event.key==='Escape') {
         calculo.numero1=0;
         calculo.numero2=null;
@@ -101,132 +101,80 @@ const handleBackspace =(event) => {
     }
 }
 
-const boton1 = document.querySelector('#boton1');
-boton1.addEventListener('click', () => {
-    const tecla = '1';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton2 = document.querySelector('#boton2');
-boton2.addEventListener('click', () => {
-    const tecla = '2';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton3 = document.querySelector('#boton3');
-boton3.addEventListener('click', () => {
-    const tecla = '3';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton4 = document.querySelector('#boton4');
-boton4.addEventListener('click', () => {
-    const tecla = '4';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton5 = document.querySelector('#boton5');
-boton5.addEventListener('click', () => {
-    const tecla = '5';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton6 = document.querySelector('#boton6');
-boton6.addEventListener('click', () => {
-    const tecla = '6';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton7 = document.querySelector('#boton7');
-boton7.addEventListener('click', () => {
-    const tecla = '7';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton8 = document.querySelector('#boton8');
-boton8.addEventListener('click', () => {
-    const tecla = '8';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton9 = document.querySelector('#boton9');
-boton9.addEventListener('click', () => {
-    const tecla = '9';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const boton0 = document.querySelector('#boton0');
-boton0.addEventListener('click', () => {
-    const tecla = '0';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonMas = document.querySelector('#botonMas');
-botonMas.addEventListener('click', () => {
-    const tecla = '+';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonMenos = document.querySelector('#botonMenos');
-botonMenos.addEventListener('click', () => {
-    const tecla = '-';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonx = document.querySelector('#botonx');
-botonx.addEventListener('click', () => {
-    const tecla = '*';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonDivision = document.querySelector('#botonDivision');
-botonDivision.addEventListener('click', () => {
-    const tecla = '/';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonResto = document.querySelector('#botonResto');
-botonResto.addEventListener('click', () => {
-    const tecla = '%';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-
-const botonIgual = document.querySelector('#botonIgual');
-botonIgual.addEventListener('click', () => {
-    const tecla = '=';
-    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonc = document.querySelector('#botonc');
-botonc.addEventListener('click', () => {
-    const tecla = 'Escape';
-    const eventoKeyPress = new KeyboardEvent("keydown", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
-
-const botonBorrar = document.querySelector('#botonBorrar');
-botonBorrar.addEventListener('click', () => {
-    const tecla = 'Backspace';
-    const eventoKeyPress = new KeyboardEvent("keydown", { key: tecla});
-    document.dispatchEvent(eventoKeyPress);
-});
+const handleClick = (event) => {
+    let tecla = null;
+    let teclaDown = null;
+    switch (event.target.id) {
+        case 'boton0':
+            tecla = '0';
+            break;
+        case 'boton1':
+            tecla = '1';
+            break;
+        case 'boton2':
+            tecla = '2';
+            break;
+        case 'boton3':
+            tecla = '3';
+            break;
+        case 'boton4':
+            tecla = '4';
+            break;
+        case 'boton5':
+            tecla = '5';
+            break;
+        case 'boton6':
+            tecla = '6';
+            break;
+        case 'boton7':
+            tecla = '7';
+            break;
+        case 'boton8':
+            tecla = '8';
+            break;
+        case 'boton9':
+            tecla = '9';
+            break;
+        case 'botonIgual':
+            tecla = 'Enter'
+            break;
+        case 'botonResto':
+            tecla = '%';
+            break;
+        case 'botonx':
+            tecla = '*';
+            break;
+        case 'botonMenos':
+            tecla = '-';
+            break;
+        case 'botonMas':
+            tecla = '+';
+            break;
+        case 'botonDivision':
+            tecla = '/';
+            break;
+        case 'botonIgual':
+            tecla = '=';
+            break
+        case 'botonc':
+            teclaDown = 'Escape';
+            break;
+        case 'botonBorrar':
+            teclaDown = 'Backspace';
+            break;
+        default:
+            break;
+    }
+    if (tecla!==null) {
+        const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+        document.dispatchEvent(eventoKeyPress);
+    }
+    if (teclaDown!==null) {
+        const eventoKeyDown = new KeyboardEvent("keydown", { key: teclaDown});
+        document.dispatchEvent(eventoKeyDown);
+    }
+}
 
 document.addEventListener('keypress', handleKeyPress);
-document.addEventListener('keydown', handleBackspace);
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('click', handleClick);
---
+++ js/scriptOld.js
@@ -0,0 +1,129 @@
+/* Este es el codigo no optimizado para el manejo del evento click de los botones
+
+
+const boton1 = document.querySelector('#boton1');
+boton1.addEventListener('click', () => {
+    const tecla = '1';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+}); 
+
+const boton2 = document.querySelector('#boton2');
+boton2.addEventListener('click', () => {
+    const tecla = '2';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton3 = document.querySelector('#boton3');
+boton3.addEventListener('click', () => {
+    const tecla = '3';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton4 = document.querySelector('#boton4');
+boton4.addEventListener('click', () => {
+    const tecla = '4';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton5 = document.querySelector('#boton5');
+boton5.addEventListener('click', () => {
+    const tecla = '5';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton6 = document.querySelector('#boton6');
+boton6.addEventListener('click', () => {
+    const tecla = '6';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton7 = document.querySelector('#boton7');
+boton7.addEventListener('click', () => {
+    const tecla = '7';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton8 = document.querySelector('#boton8');
+boton8.addEventListener('click', () => {
+    const tecla = '8';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const boton9 = document.querySelector('#boton9');
+boton9.addEventListener('click', () => {
+    const tecla = '9';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+/* const boton0 = document.querySelector('#boton0');
+boton0.addEventListener('click', () => {
+    const tecla = '0';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+}); 
+
+const botonMas = document.querySelector('#botonMas');
+botonMas.addEventListener('click', () => {
+    const tecla = '+';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const botonMenos = document.querySelector('#botonMenos');
+botonMenos.addEventListener('click', () => {
+    const tecla = '-';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const botonx = document.querySelector('#botonx');
+botonx.addEventListener('click', () => {
+    const tecla = '*';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const botonDivision = document.querySelector('#botonDivision');
+botonDivision.addEventListener('click', () => {
+    const tecla = '/';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const botonResto = document.querySelector('#botonResto');
+botonResto.addEventListener('click', () => {
+    const tecla = '%';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+
+const botonIgual = document.querySelector('#botonIgual');
+botonIgual.addEventListener('click', () => {
+    const tecla = '=';
+    const eventoKeyPress = new KeyboardEvent("keypress", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+}); 
+
+const botonc = document.querySelector('#botonc');
+botonc.addEventListener('click', () => {
+    const tecla = 'Escape';
+    const eventoKeyPress = new KeyboardEvent("keydown", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+});
+
+const botonBorrar = document.querySelector('#botonBorrar');
+botonBorrar.addEventListener('click', () => {
+    const tecla = 'Backspace';
+    const eventoKeyPress = new KeyboardEvent("keydown", { key: tecla});
+    document.dispatchEvent(eventoKeyPress);
+}); */
